74194d1cdc78c551e23b8597d6e431a27a4c0ac9
Party4Bread <party4bread@gmail.com>
2017-11-29 23:50:20 +0900

    Complete Bubblesort and Selectionsort

---
+++ SortAlgorithms/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms
+{
+    public static class BubbleSort
+    {
+        public static long[] Sort(long[] srcList)
+        {
+            long temp = 0;
+            for (int write = 0; write < srcList.Length; write++)
+            {
+                for (int sort = 0; sort < srcList.Length - 1; sort++)
+                {
+                    if (srcList[sort] > srcList[sort + 1])
+                    {
+                        (srcList[sort],srcList[sort + 1]) = (srcList[sort + 1],srcList[sort]);
+                    }
+                }
+            }
+            return srcList;
+        }
+    }
+}
---
+++ SortAlgorithms/SelectionSort.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SortAlgorithms
+{
+    public static class SelectionSort
+    {
+        public static long[] Sort(long[] srcList)
+        {
+            for (int i = 0; i<srcList.Length-1; i++)
+            {
+                int indexMin = i;
+                for (int j = i+1; j < srcList.Length; j++)
+                {
+                    if (srcList[indexMin] > srcList[j])
+                    {
+                        indexMin = j;
+                    }
+                }
+                (srcList[indexMin], srcList[i]) = (srcList[i], srcList[indexMin]);
+            }
+            return srcList;
+        }
+    }
+}
